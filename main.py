63e1288632047b684eaccdb2097a26e5e812c0a4
Bradon Kanyid <rattboi@gmail.com>
2014-01-20 23:53:57 -0800

    Working for single search term. A little more organized. Parameterized better

--- main.py
+++ main.py
@@ -1,57 +1,48 @@
+#!/usr/bin/env python
 from bs4 import BeautifulSoup
 import urllib2
 import requests
 import sys
 import re
 import getpass
+import json
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
     sys.exit(1);
+else:
+    username = sys.argv[1]
+    searchterm = ' '.join(sys.argv[2:])
 
+songkick_base_url = "https://www.songkick.com"
 basesessionurl = "https://www.songkick.com/session/"
 newloginurl    = basesessionurl + "new"
 createloginurl = basesessionurl + "create"
 basequerystring = "https://www.songkick.com/search?page=1&per_page=10&type=artists&query="
-queryurl = basequerystring + urllib2.quote(sys.argv[1])
+queryurl = basequerystring + urllib2.quote(searchterm)
 
-re_authkey = re.compile("input name=\"authenticity_token\" type=\"hidden\" value=\"(.*)\"")
+re_auth_token = re.compile("input name=\"authenticity_token\" type=\"hidden\" value=\"(.*)\"")
 
-password = getpass.getpass()
-
-# create session for SongKick
-s = requests.session()
-
-# get initial login page to get authenticity token
-newlogin = s.get(newloginurl)
-auth_key = re_authkey.findall(newlogin.content)[0]
-
-# add authenticity token to login info, and login
-payload = {'authenticity_token': auth_key,
-           'cancel_url': '/',
-           'success_url': '/',
-           'username_or_email': 'rattboi',
-           'password': password,
-           'persist': 'y',
-           'commit': 'Log in'}
-r = s.post(createloginurl, data=payload)
-
-# Get artists related to given CLI argument
-artistspage = s.get(queryurl)
-artist_soup = BeautifulSoup(artistspage.content)
+headers = {'Content-Type':'application/json','Accept':'*/*','X-Requested-With':'XMLHttpRequest'}
 
 def get_artists(soup):
     artist_names    = soup.select('li.artist > p.subject > a')
     artist_tracking = soup.select('input.artist')
-    return [(artist_names[i].text, artist_tracking[i].attrs['value']) for i in range(len(artist_names))]
+    artist_form     = soup.select('li.artist > div > div > form')
+    return [(artist_names[i].text, 
+             artist_tracking[i].attrs['value'], 
+             artist_form[i].attrs['action'],
+             get_attrs(artist_form[i])) 
+             for i in range(len(artist_names))]
 
-artists = get_artists(artist_soup)
+def get_attrs(elem):
+    form_attrs = filter(lambda x: x.has_attr('name'), elem.select('input'))
+    return [(x.attrs['name'],x.attrs['value']) for x in form_attrs]
 
-if any(filter(lambda (x,y): y == u'Stop tracking', artists)):
-    print "Already tracking"
-else:
-    distances = [levenshtein(a,sys.argv[1]) for (a,b) in artists]
-    val, idx = min((val, idx) for (idx, val) in enumerate(distances))
-    #now we have the index of our most likely candidate. "Track it"
+def track_artist(s, artist_to_track):
+    attr_map = {}
+    for (key,val) in artist_to_track[3]:
+        attr_map[key] = val
+    r = s.post(songkick_base_url + artist_to_track[2], data=json.dumps(attr_map), headers=headers)
 
 def levenshtein(a,b):
     "Calculates the Levenshtein distance between a and b."
@@ -72,3 +63,64 @@ def levenshtein(a,b):
             current[j] = min(add, delete, change)
 
     return current[n]
+
+def do_login(username, password):
+    # create session for SongKick
+    s = requests.session()
+
+    # get initial login page to get authenticity token
+    try:
+        r = s.get(newloginurl)
+    except:
+        print "Couldn't get to login page"
+        return None
+
+    try:
+        auth_key = re_auth_token.findall(r.content)[0]
+    except:
+        print "Couldn't find auth token in login page"
+        return None
+
+    # add authenticity token to login info, and login
+    payload = {'authenticity_token': auth_key,
+               'cancel_url': '/',
+               'success_url': '/',
+               'username_or_email': username,
+               'password': password,
+               'persist': 'y',
+               'commit': 'Log in'}
+    try:
+        r = s.post(createloginurl, data=payload)
+    except:
+        print "Couldn't log in"
+        return None
+
+    if r.status_code != 200:
+        print "Some error logging in"
+        return None
+    else:
+        return s
+
+def search_for_artist(s, queryurl):
+    # Get artists related to given CLI argument
+    artistspage = s.get(queryurl)
+    artist_soup = BeautifulSoup(artistspage.content)
+    return artist_soup
+
+def attempt_to_track(s, artists):
+    if len(artists) == 0:
+        print "No Results"
+    elif any(filter(lambda (n,t,u,a): t == u'Stop tracking', artists)):
+        print "Already tracking"
+    else:
+        distances = [levenshtein(n,searchterm) for (n,t,u,attrs) in artists]
+        val, idx = min((val, idx) for (idx, val) in enumerate(distances))
+        #now we have the index of our most likely candidate. "Track it"
+        track_artist(s, artists[idx])
+
+password = getpass.getpass()
+
+s = do_login(username, password)
+artist_soup = search_for_artist(s, queryurl)
+artists = get_artists(artist_soup)
+attempt_to_track(s, artists)
