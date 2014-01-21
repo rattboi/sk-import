ee674f9c40e3f90bf62361de5fee1607ce1ecb8a
Bradon Kanyid <rattboi@gmail.com>
2014-01-20 19:44:41 -0800

    Now it's listing artists. Next step: figure out how to post correctly

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 import urllib2
 import requests
 import sys
@@ -7,17 +8,24 @@ import getpass
 if len(sys.argv) < 2:
     sys.exit(1);
 
+basesessionurl = "https://www.songkick.com/session/"
+newloginurl    = basesessionurl + "new"
+createloginurl = basesessionurl + "create"
+basesearchstring = "https://www.songkick.com/search?page=1&per_page=10&type=artists&query="
+queryurl = basesearchstring + urllib2.quote(sys.argv[1])
+
 re_authkey = re.compile("input name=\"authenticity_token\" type=\"hidden\" value=\"(.*)\"")
 
 password = getpass.getpass()
 
-newloginurl    = "https://www.songkick.com/session/new"
-createloginurl = "https://www.songkick.com/session/create"
+# create session for SongKick
+s = requests.session()
 
-newlogin = requests.get(newloginurl)
-
+# get initial login page to get authenticity token
+newlogin = s.get(newloginurl)
 auth_key = re_authkey.findall(newlogin.content)[0]
 
+# add authenticity token to login info, and login
 payload = {'authenticity_token': auth_key,
            'cancel_url': '/',
            'success_url': '/',
@@ -25,16 +33,12 @@ payload = {'authenticity_token': auth_key,
            'password': password,
            'persist': 'y',
            'commit': 'Log in'}
+r = s.post(createloginurl, data=payload)
 
-r = requests.post(createloginurl, data=payload, cookies=newlogin.cookies)
+# Get artists related to given CLI argument 
+artists = s.get(queryurl)
+artist_soup = BeautifulSoup(artists.content)
 
-basesearchstring = "https://www.songkick.com/search?page=1&per_page=10&type=artists&query="
-queryurl = basesearchstring + urllib2.quote(sys.argv[1])
-
-'''
-testquery = requests.get(queryurl, cookies=r.cookies)
-
-f = open('somefile.htm', 'w')
-f.writelines(testquery.content)
-f.close()
-'''
+artist_list = artist_soup.select('li.artist > p.subject > a')
+for a in artist_list:
+    print a.text
